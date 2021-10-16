fa179b221ba7acf6f5e065fde253fb61601f477a
Beggiice <beggiice@gmail.com>
2021-10-16 08:15:25 +0000

    Final 2

--- .idea/libraries/Assignment 4.1.dart
+++ .idea/libraries/Assignment 4.1.dart
@@ -8,6 +8,10 @@ List<int> calculate_sum = [645,64,12,8,87,63,13,84,988,732,1,18,968,47,321];
 
 void main() {
 
-print(calculate_sum.length);
+    int sum = 0;
 
+    for (int i = 1; i < calculate_sum.length; i++) {
+    sum += calculate_sum[i];
+    }
+    print(sum);
 }
